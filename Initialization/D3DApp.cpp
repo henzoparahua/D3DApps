7145ec9636ec8361c7b07e0e604d534e969dda82
henzoparahua <hnz.2894henzop19@gmail.com>
2026-04-01 19:56:13 -0300

    changes to the chk and removed the triqangle sample, going to take a new approach

--- Initialization/D3DApp.cpp
+++ Initialization/D3DApp.cpp
@@ -20,39 +20,36 @@ void D3DApp::LoadPipeline()
 
 #if defined(_DEBUG)
     {
-        ComPtr<ID3D12Debug> debugController;
-        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
-        {
-            debugController->EnableDebugLayer();
-
-            dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
-        }
+        ComPtr<ID3D12Debug1> debugController;
+        D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)) >> chk;
+        debugController->EnableDebugLayer();
+        debugController->SetEnableGPUBasedValidation(true);
     }
 #endif
 
     ComPtr<IDXGIFactory7> factory;
-    ThrowIfFailed(CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&factory)));
+    CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&factory)) >> chk;
 
     if (m_useWarpDevice)
     {
         ComPtr<IDXGIAdapter> warpAdapter;
-        ThrowIfFailed(factory->EnumWarpAdapter(IID_PPV_ARGS(&warpAdapter)));
+        factory->EnumWarpAdapter(IID_PPV_ARGS(&warpAdapter)) >> chk;
 
-        ThrowIfFailed(D3D12CreateDevice(
+        D3D12CreateDevice(
             warpAdapter.Get(),
             D3D_FEATURE_LEVEL_12_1,
             IID_PPV_ARGS(&m_device)
-        ));
+        ) >> chk;
     }
     else {
         ComPtr<IDXGIAdapter1> hardwareAdapter;
         GetHardwareAdapter(factory.Get(), &hardwareAdapter);
 
-        ThrowIfFailed(D3D12CreateDevice(
+        D3D12CreateDevice(
             hardwareAdapter.Get(),
             D3D_FEATURE_LEVEL_12_1,
             IID_PPV_ARGS(&m_device)
-        ));
+        ) >> chk;
     }
 
     //  Create and Describe the Command Queue
@@ -60,7 +57,7 @@ void D3DApp::LoadPipeline()
     queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
     queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
 
-    ThrowIfFailed(m_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_commandQueue)));
+    m_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_commandQueue)) >> chk;
 
     //  Describe and create the Swap Chain
     DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
@@ -73,19 +70,19 @@ void D3DApp::LoadPipeline()
     swapChainDesc.SampleDesc.Count = 1;
 
     ComPtr<IDXGISwapChain1> swapChain;
-    ThrowIfFailed(factory->CreateSwapChainForHwnd(
+    factory->CreateSwapChainForHwnd(
         m_commandQueue.Get(),
         Win32App::GetHwnd(),
         &swapChainDesc,
         nullptr,
         nullptr,
         &swapChain
-    ));
+    ) >> chk;
 
 
-    ThrowIfFailed(factory->MakeWindowAssociation(Win32App::GetHwnd(), DXGI_MWA_NO_ALT_ENTER));
+    factory->MakeWindowAssociation(Win32App::GetHwnd(), DXGI_MWA_NO_ALT_ENTER) >> chk;
 
-    ThrowIfFailed(swapChain.As(&m_swapChain));
+    swapChain.As(&m_swapChain) >> chk;
     frameIndex = m_swapChain->GetCurrentBackBufferIndex();
 
     //  Create Descriptor Heap
@@ -95,7 +92,7 @@ void D3DApp::LoadPipeline()
         rtvHeapDesc.NumDescriptors = NumFrames;
         rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
         rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
-        ThrowIfFailed(m_device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&m_rtvHeap)));
+        m_device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&m_rtvHeap)) >> chk;
 
         m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
     }
@@ -107,31 +104,31 @@ void D3DApp::LoadPipeline()
         //  Create a RTV for each frame.
         for (UINT n{ 0 }; n < NumFrames; n++)
         {
-            ThrowIfFailed(m_swapChain->GetBuffer(n, IID_PPV_ARGS(&m_renderTargets[n])));
+            m_swapChain->GetBuffer(n, IID_PPV_ARGS(&m_renderTargets[n])) >> chk;
             m_device->CreateRenderTargetView(m_renderTargets[n].Get(), nullptr, rtvHandle);
             rtvHandle.Offset(1, m_rtvDescriptorSize);
         }
     }
 
-    ThrowIfFailed(m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator)));
+    m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator)) >> chk;
 }
 
 void D3DApp::LoadAssets()
 {
     //  Create the Command List
-    ThrowIfFailed(m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocator.Get(), nullptr, IID_PPV_ARGS(&m_commandList)));
+    m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocator.Get(), nullptr, IID_PPV_ARGS(&m_commandList)) >> chk;
 
-    ThrowIfFailed(m_commandList->Close());
+    m_commandList->Close() >> chk;
 
     //  Create Synchronization Objects
     {
-        ThrowIfFailed(m_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence)));
+        m_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence)) >> chk;
         fenceValue = 1;
 
         fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
         if (fenceEvent == nullptr)
         {
-            ThrowIfFailed(HRESULT_FROM_WIN32(GetLastError()));
+            HRESULT_FROM_WIN32(GetLastError()) >> chk;
         }
     }
 }
@@ -150,7 +147,7 @@ void D3DApp::OnRender()
     ID3D12CommandList* ppCommandList[] = { m_commandList.Get() };
     m_commandQueue->ExecuteCommandLists(_countof(ppCommandList), ppCommandList);
 
-    ThrowIfFailed(m_swapChain->Present(1, 0));
+    m_swapChain->Present(1, 0) >> chk;
 
     WaitForPreviousFrame();
 }
@@ -165,9 +162,9 @@ void D3DApp::OnDestroy()
 
 void D3DApp::PopulateCommandList()
 {
-    ThrowIfFailed(m_commandAllocator->Reset());
+    m_commandAllocator->Reset() >> chk;
 
-    ThrowIfFailed(m_commandList->Reset(m_commandAllocator.Get(), m_pipelineState.Get()));
+    m_commandList->Reset(m_commandAllocator.Get(), m_pipelineState.Get()) >> chk;
 
     //  Indicate that the back buffer will be used as a render target.
     {
@@ -197,7 +194,7 @@ void D3DApp::PopulateCommandList()
         m_commandList->ResourceBarrier(1, &rtvState);
     }
 
-    ThrowIfFailed(m_commandList->Close());
+    m_commandList->Close() >> chk;
 }
 
 void D3DApp::WaitForPreviousFrame()
@@ -206,12 +203,12 @@ void D3DApp::WaitForPreviousFrame()
 
     //  Signal and increment the fence value.
     const UINT64 fence{ fenceValue };
-    ThrowIfFailed(m_commandQueue->Signal(m_fence.Get(), fence));
+    m_commandQueue->Signal(m_fence.Get(), fence) >> chk;
     fenceValue++;
 
     if (m_fence->GetCompletedValue() < fence)
     {
-        ThrowIfFailed(m_fence->SetEventOnCompletion(fence, fenceEvent));
+        m_fence->SetEventOnCompletion(fence, fenceEvent) >> chk;
         WaitForSingleObject(fenceEvent, INFINITE);
     }
 
